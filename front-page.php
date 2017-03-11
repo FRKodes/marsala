ac7b92618cb5089a13db916b5acb1b1e5b22eefd
frkodes <frkalderon@gmail.com>
2017-03-10 18:46:42 -0600

    The first one

---
+++ front-page.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The Front Page template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package MARSALA
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="row">
+				<div class="main-banners-container">
+					<div class="item"></div>
+				</div>
+			</div>
+		
+			<div class="container">
+				<div class="row">
+					<?php if ( have_posts() ) :
+
+						get_template_part( 'loop' );
+
+					else :
+
+						get_template_part( 'content', 'none' );
+
+					endif; ?>
+				</div>
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+// do_action( 'storefront_sidebar' );
+get_footer();
